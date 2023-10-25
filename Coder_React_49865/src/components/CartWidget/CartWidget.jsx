8a7c4bdaf1eddb4d58b589b32e4ce366cc06fc80
Blanc Luis Maximiliano <machys1985@gmail.com>
2023-10-25 16:38:12 -0300

    incorporando elementos de entrega

---
+++ Coder_React_49865/src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BsCart4 } from "react-icons/bi";
+
+const CartWidget=()=>{
+    return(
+        <>
+            <BsCart4/>
+            <p>0</p>
+        </>
+    )
+}
+
+export default CartWidget;
